be3b386024f8fc110a93ca6671f759e22ee1168c
alperaltuntas <alperaltuntas@gmail.com>
2019-01-17 12:52:05 -0700

    cleanup (intermediate)

--- mavg.py
+++ mavg.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
-""" Generates weighed monthly avg files from given history files
-    that have more frequently recorded data.
+""" Generates weighed monthly avg files from given history files with more frequently recorded data.
+    The monthly files will be saved with a prefix "mavg_".
+    After having checked the average files are ok, you can remove the prefix from all files at once
+    by running the following linux command (Warning: This will override any existing file, averaged or not!):
+        rename "mavg_" "" mavg_*
 """
 
 from __future__ import print_function
@@ -20,9 +23,6 @@ parser.add_argument('-f', metavar='path', type=str, required=True, nargs='*',
 parser.add_argument('-x', metavar='excl', type=str, required=False,
                     help='(optional). File names that have the string provided after this flag'
                          ' will be discarded. ')
-parser.add_argument('-c', metavar='comp', type=str, required=False,
-                    help='(optional). 3-character CESM component name that generates the history '
-                    'files. If not provided, the script will try to determine this from file names.')
 parser.add_argument('-v', action='store_true', required=False, help='Verbose logging')
 args = parser.parse_args()
 
@@ -34,7 +34,7 @@ glob = GlobalData()
 
 def preprocess_out_files():
     """ Extracts some general information about the averaging files to generate. Mainly, determines
-        which files to read and weights for each averaging file """
+        which files to read and weights for each averaging file. Also returns avg_chunks """
 
     if args.v and comm.Get_rank()==0:
         print("pre-processing files")
@@ -43,7 +43,7 @@ def preprocess_out_files():
     date0_out = add_months(glob.date0_out, min(glob.m_per_proc*(comm.Get_rank()),glob.nmonths))
     date1_out = add_months(glob.date0_out, min(glob.m_per_proc*(comm.Get_rank()+1),glob.nmonths))
 
-    # determine the months to process for this proc:
+    # determine the averaging chunks (months) to process for this proc:
     avg_chunks = []
     chunk_date0 = date0_out # first chunk's date0
     while True:
@@ -52,48 +52,55 @@ def preprocess_out_files():
             avg_chunks.append( AvgChunk(chunk_date0,"month",glob.fprefix, glob.fsuffix) )
             chunk_date0 = chunk_date1 # move to next potential chunk
         else:
+            # the end of this chunk is beyond the extent of input files.
+            # do not add this chunk and break here.
             break
 
     fi = 0 # file index
-    for avg_chunk in avg_chunks:
+    for chunk in avg_chunks:
 
         chunk_preprocessed = False
         while not chunk_preprocessed:
 
             # Read the input file
-            ncfile_in = xr.open_dataset(glob.files[fi],decode_times=False)
-            day0_in = ncfile_in.time_bound.data[0][0]
-            day1_in = ncfile_in.time_bound.data[-1][1]
-            date0_in = nc4.num2date(day0_in, glob.nc_dtime_unit, glob.nc_calendar)
-            date1_in = nc4.num2date(day1_in, glob.nc_dtime_unit, glob.nc_calendar)
+            with xr.open_dataset(glob.files[fi],decode_times=False, cache=False, decode_cf=False) as ncfile_in:
+                day0_in = ncfile_in.time_bound.data[0][0]   # beginning day of this input file
+                day1_in = ncfile_in.time_bound.data[-1][1]  # ending day of this input file
+                date0_in = nc4.num2date(day0_in, glob.nc_dtime_unit, glob.nc_calendar) # beginning date of this file
+                date1_in = nc4.num2date(day1_in, glob.nc_dtime_unit, glob.nc_calendar) # ending date of this file
 
-            if (date1_in <= avg_chunk.date0): # Not reached to the chunks of this proc yet. keep reading
-                pass
+                if (date1_in <= chunk.date0):   # The dates of this file (fi) precede the time interval that
+                                                # this proc is responsible for. So, move on to next file. (fi++)
+                    pass
 
-            elif (date0_in >= avg_chunk.date1): # Already passed the current chunk
-                chunk_preprocessed = True
+                elif (date0_in >= chunk.date1): # The dates of this file (fi) are later than the time interval that
+                                                # this file (fi) covers. so, stop reading files.
+                    chunk_preprocessed = True
 
-            elif (date0_in <= avg_chunk.date0 and date1_in >  avg_chunk.date0) or \
-                 (date0_in >= avg_chunk.date0 and date1_in <= avg_chunk.date1) or \
-                 (date0_in <  avg_chunk.date1 and date1_in >= avg_chunk.date1):
-                # The current file has information for this avg chunk
+                elif (date0_in <= chunk.date0 and date1_in >  chunk.date0) or \
+                     (date0_in >= chunk.date0 and date1_in <= chunk.date1) or \
+                     (date0_in <  chunk.date1 and date1_in >= chunk.date1):
+                    # The dates of this file (fi) falls within the time interval that this proc is responsible for.
+                    # so, read in this file.
 
-                # Determine the weights of each input file
-                ndays_within_month = ( min(avg_chunk.date1,date1_in)-max(avg_chunk.date0,date0_in) ).days
-                weight = float(ndays_within_month)/avg_chunk.ndays
-                #if args.v and comm.Get_rank()==1: print ("\trank: ",comm.Get_rank(), " reading",date0_in,ndays_within_month,weight)
-                avg_chunk.in_files.append([glob.files[fi], weight])
+                    # Determine the weight of this file (fi):
+                    ndays_within_chunk = ( min(chunk.date1,date1_in)-max(chunk.date0,date0_in) ).days
+                    weight = float(ndays_within_chunk)/chunk.ndays
+                    chunk.in_files.append([glob.files[fi], weight])
 
-            else:
-                print(date0_in, date1_in, avg_chunk.date0, avg_chunk.date1)
-                raise RuntimeError("Error detected in the file read/write logic", comm.Get_rank())
+                else:
+                    print(date0_in, date1_in, chunk.date0, chunk.date1)
+                    raise RuntimeError("Error detected in the file read/write logic", comm.Get_rank())
 
-            # check if this was the final file to read for this month
-            if date1_in>=avg_chunk.date1:
-                chunk_preprocessed=True
+                # check if this was the final file to read for this chunk
+                if date1_in>=chunk.date1:
+                    chunk_preprocessed=True
 
-            fi = fi+1 # skip to next file to read, which may have information for this month
-        fi = fi-1 # skip to next month to write (and move to previous file in case it had information for the next mth)
+            fi = fi+1 # skip to next file to read, which may have information for this chunk (month)
+
+        # Done preprocessing this chunk (month). Move to the next chunk
+        fi = fi-1 # (and move to previous file in case it had information for the next chunk)
+
     return avg_chunks
 
 def process_out_files(avg_chunks):
@@ -104,12 +111,12 @@ def process_out_files(avg_chunks):
 
     comm.Barrier()
 
-    for avg_chunk in avg_chunks:
+    for chunk in avg_chunks:
         if args.v:
-            print("rank:",comm.Get_rank(), " \tprocessing", avg_chunks.index(avg_chunk)+1, "of", len(avg_chunks))
+            print("rank:",comm.Get_rank(), " \tprocessing", avg_chunks.index(chunk)+1, "of", len(avg_chunks))
 
         # instantiate the first input file to get some general information and time-independent arrays:
-        in_ds0 = xr.open_dataset(avg_chunk.in_files[0][0], decode_times=False, cache=False)
+        in_ds0 = xr.open_dataset(chunk.in_files[0][0], decode_times=False, cache=False, decode_cf=False)
 
         # instantiate the file to write
         with xr.Dataset(coords=in_ds0.coords, attrs=in_ds0.attrs) as out_ds:
@@ -119,7 +126,7 @@ def process_out_files(avg_chunks):
             for da in in_ds0.variables:
                 if not "time" in in_ds0[da].dims:
                     out_ds[da] = in_ds0[da]
-            out_ds.to_netcdf(path=avg_chunk.out_filename, mode='w',unlimited_dims=["time"])
+            out_ds.to_netcdf(path=chunk.out_filename, mode='w',unlimited_dims=["time"])
 
         # now compute and fill in variables with "time" dimension
         # (weighted averaging)
@@ -132,7 +139,7 @@ def process_out_files(avg_chunks):
                 # such that the dataset stores only one variable in primary memory at a time
                 with xr.Dataset(coords=in_ds0.coords, attrs=in_ds0.attrs) as out_ds:
 
-                    for in_file in avg_chunk.in_files:
+                    for in_file in chunk.in_files:
                         in_filename = in_file[0]
                         weight      = in_file[1]
                         with xr.open_dataset(in_filename, decode_times=False) as in_ds:
@@ -141,7 +148,7 @@ def process_out_files(avg_chunks):
                             else: # accumulate
                                 out_ds[da].data = out_ds[da].data + in_ds[da].data*weight
 
-                    out_ds.to_netcdf(path=avg_chunk.out_filename, mode='a')
+                    out_ds.to_netcdf(path=chunk.out_filename, mode='a')
 
 
 def main():
